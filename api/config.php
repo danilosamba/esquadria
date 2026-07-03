4cd0fa2e7e58cc6decdbf4c9522b5042c2d7969c
Danilo_Note <dh.miranda.dm@gmail.com>
2026-07-03 11:54:52 -0300

    config server

--- api/config.php
+++ api/config.php
@@ -2,7 +2,7 @@
 // api/config.php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'inoves66_luzorcamentos');
-define('DB_USER', 'inoves66_luzorcamentos');
-define('DB_PASS', 'U55wgan@325449');
+define('DB_NAME', 'inoves66_esquadria');
+define('DB_USER', 'inoves66_esquadria');
+define('DB_PASS', 'U55wgan@');
 define('JWT_SECRET', 'luz-super-secret-key-2026');
